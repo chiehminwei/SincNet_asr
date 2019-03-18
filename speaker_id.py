618cf56c4e3dd98a8b2b056cff32f4008bbddadd
Chiehmin Wei <cmw025@bucknell.edu>
2019-03-18 12:02:07 +0000

    shii

--- speaker_id.py
+++ speaker_id.py
@@ -199,12 +199,149 @@ DNN2_arch = {'input_dim':fc_lay[-1] ,
 DNN2_net=MLP(DNN2_arch)
 DNN2_net.cuda()
 
+subprocess.call(['gsutil', 'cp', 'gs://edinquake/asr/SincNet_TIMIT_window/model_best.pkl', 'model_best.pkl'], stdout=FNULL, stderr=subprocess.STDOUT)
+checkpoint_load = torch.load('model_best.pkl', map_location=lambda s, l: default_restore_location(s, 'cpu'))
+CNN_net.load_state_dict(checkpoint_load['CNN_model_par'])
+DNN1_net.load_state_dict(checkpoint_load['DNN1_model_par'])
+DNN2_net.load_state_dict(checkpoint_load['DNN2_model_par'])
+best_validate = float('inf')
+last_epoch = -1
 
 if pt_file!='none':
-   checkpoint_load = torch.load(pt_file)
-   CNN_net.load_state_dict(checkpoint_load['CNN_model_par'])
-   DNN1_net.load_state_dict(checkpoint_load['DNN1_model_par'])
-   DNN2_net.load_state_dict(checkpoint_load['DNN2_model_par'])
+  checkpoint_load = torch.load(pt_file)
+  CNN_net.load_state_dict(checkpoint_load['CNN_model_par'])
+  DNN1_net.load_state_dict(checkpoint_load['DNN1_model_par'])
+  DNN2_net.load_state_dict(checkpoint_load['DNN2_model_par'])
+  last_epoch = checkpoint_load['epoch']
+  best_validate = checkpoint_load['best_validate']
+
+  if dvector!='none':
+    #!/usr/bin/env python3
+    # -*- coding: utf-8 -*-
+    """
+    Created on Wed Dec 19 14:34:01 2018
+
+    @author: Harry
+
+    Creates "segment level d vector embeddings" compatible with
+    https://github.com/google/uis-rnn
+
+    """
+    from torch.serialization import default_restore_location
+    import glob
+    import librosa
+    import numpy as np
+    import os
+    import torch
+
+    from hparam import hparam as hp
+    from speech_embedder_net import SpeechEmbedder
+    from VAD_segments import VAD_chunk
+    import subprocess
+
+    def concat_segs(times, segs):
+      #Concatenate continuous voiced segments
+      concat_seg = []
+      seg_concat = segs[0]
+      for i in range(0, len(times)-1):
+        if times[i][1] == times[i+1][0]:
+          seg_concat = np.concatenate((seg_concat, segs[i+1]))
+        else:
+          concat_seg.append(seg_concat)
+          seg_concat = segs[i+1]
+      else:
+        concat_seg.append(seg_concat)
+      return concat_seg
+
+    def get_STFTs(segs):
+      #Get 240ms STFT windows with 50% overlap
+      sr = hp.data.sr
+      STFT_frames = []
+      for seg in segs:
+        S = librosa.core.stft(y=seg, n_fft=hp.data.nfft,
+                              win_length=int(hp.data.window * sr), hop_length=int(hp.data.hop * sr))
+        S = np.abs(S)**2
+        mel_basis = librosa.filters.mel(sr, n_fft=hp.data.nfft, n_mels=hp.data.nmels)
+        S = np.log10(np.dot(mel_basis, S) + 1e-6)           # log mel spectrogram of utterances
+        for j in range(0, S.shape[1], int(.12/hp.data.hop)):
+          if j + 24 < S.shape[1]:
+            STFT_frames.append(S[:,j:j+24])
+          else:
+            break
+      return STFT_frames
+
+    def align_embeddings(embeddings):
+      partitions = []
+      start = 0
+      end = 0
+      j = 1
+      for i, embedding in enumerate(embeddings):
+        if (i*.12)+.24 < j*.401:
+          end = end + 1
+        else:
+          partitions.append((start,end))
+          start = end
+          end = end + 1
+          j += 1
+      else:
+        partitions.append((start,end))
+      avg_embeddings = np.zeros((len(partitions),256))
+      for i, partition in enumerate(partitions):
+        avg_embeddings[i] = np.average(embeddings[partition[0]:partition[1]],axis=0) 
+      return avg_embeddings
+    
+    #dataset path
+    unprocessed_data = 'wtf_timit/*/*/*/*.wav'
+    audio_path = glob.glob(os.path.dirname(unprocessed_data))  
+
+    total_speaker_num = len(audio_path)
+    train_speaker_num= (total_speaker_num//10)*9            # split total data 90% train and 10% test
+
+    embedder_net = SpeechEmbedder()
+    FNULL = open(os.devnull, 'w')
+    subprocess.call(['gsutil', 'cp', 'gs://edinquake/asr/baseline_TIMIT/model_best.pkl', hp.model.model_path], stdout=FNULL, stderr=subprocess.STDOUT)
+    embedder_net.load_state_dict(torch.load(hp.model.model_path, map_location=lambda s, l: default_restore_location(s, 'cpu')))
+    embedder_net.eval()
+
+    train_sequence = []
+    train_cluster_id = []
+    label = 0
+    count = 0 
+    train_saved = False
+    for i, folder in enumerate(audio_path):
+      for file in os.listdir(folder):
+        if file[-4:] == '.wav':
+          times, segs = VAD_chunk(2, folder+'/'+file)
+          if segs == []:
+            print('No voice activity detected')
+            continue
+          concat_seg = concat_segs(times, segs)
+          STFT_frames = get_STFTs(concat_seg)
+          STFT_frames = np.stack(STFT_frames, axis=2)
+          STFT_frames = torch.tensor(np.transpose(STFT_frames, axes=(2,1,0)))
+          embeddings = embedder_net(STFT_frames)
+          aligned_embeddings = align_embeddings(embeddings.detach().numpy())
+          train_sequence.append(aligned_embeddings)
+          for embedding in aligned_embeddings:
+            train_cluster_id.append(str(label))
+          count = count + 1
+          if count % 100 == 0:
+            print('Processed {0}/{1} files'.format(count, len(audio_path)))
+      label = label + 1
+      
+      if not train_saved and i > train_speaker_num:
+        train_sequence = np.concatenate(train_sequence,axis=0)
+        train_cluster_id = np.asarray(train_cluster_id)
+        np.save('train_sequence',train_sequence)
+        np.save('train_cluster_id',train_cluster_id)
+        train_saved = True
+        train_sequence = []
+        train_cluster_id = []
+            
+    train_sequence = np.concatenate(train_sequence,axis=0)
+    train_cluster_id = np.asarray(train_cluster_id)
+    np.save('test_sequence',train_sequence)
+    np.save('test_cluster_id',train_cluster_id)
 
 
 
@@ -212,9 +349,9 @@ optimizer_CNN = optim.RMSprop(CNN_net.parameters(), lr=lr,alpha=0.95, eps=1e-8)
 optimizer_DNN1 = optim.RMSprop(DNN1_net.parameters(), lr=lr,alpha=0.95, eps=1e-8) 
 optimizer_DNN2 = optim.RMSprop(DNN2_net.parameters(), lr=lr,alpha=0.95, eps=1e-8) 
 
-best_validate = float('inf')
+
 print('***Started training at {}***'.format(datetime.now()))
-for epoch in range(N_epochs):
+for epoch in range(last_epoch + 1, N_epochs):
   
   test_flag=0
   CNN_net.train()
@@ -323,7 +460,7 @@ for epoch in range(N_epochs):
              'best_validate': best_validate,
              }
       torch.save(checkpoint,output_folder+'/model_best.pkl')
-      subprocess.call(['gsutil', 'cp', output_folder+'/model_best.pkl', 'gs://edinquake/asr/SincNet_TIMIT/model_best.pkl'])
+      subprocess.call(['gsutil', 'cp', output_folder+'/model_best.pkl', 'gs://edinquake/asr/SincNet_TIMIT_window/model_best.pkl'])
 
 
 
